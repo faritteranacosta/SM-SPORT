847b194ed8c5b19897074031f1f06c88c2d52e0e
farit <faritteranacosta@gmail.com>
2025-11-05 21:43:15 -0500

    feat: se agregaron anotaciones necesarias para tareas asincronas y jpa

--- src/main/java/com/sm_sport/SmSportApplication.java
+++ src/main/java/com/sm_sport/SmSportApplication.java
@@ -2,8 +2,15 @@ package com.sm_sport;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
+import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
+import org.springframework.scheduling.annotation.EnableAsync;
+import org.springframework.scheduling.annotation.EnableScheduling;
 
 @SpringBootApplication
+@EnableJpaRepositories(basePackages = "com.sm_sport.repository")
+@EnableAsync // Para tareas asíncronas
+@EnableScheduling // Para tareas programadas
 public class SmSportApplication {
 
 	public static void main(String[] args) {

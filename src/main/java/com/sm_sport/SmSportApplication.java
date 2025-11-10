dc95911546445d9a50e2b56e9ef185eee73e17ed
farit <faritteranacosta@gmail.com>
2025-11-10 14:00:18 -0500

    fix: EnableConfigurationProperties

--- src/main/java/com/sm_sport/SmSportApplication.java
+++ src/main/java/com/sm_sport/SmSportApplication.java
@@ -2,6 +2,7 @@ package com.sm_sport;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.context.properties.EnableConfigurationProperties;
 import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 import org.springframework.scheduling.annotation.EnableAsync;
@@ -11,6 +12,7 @@ import org.springframework.scheduling.annotation.EnableScheduling;
 @EnableJpaRepositories(basePackages = "com.sm_sport.repository")
 @EnableAsync // Para tareas asíncronas
 @EnableScheduling // Para tareas programadas
+@EnableConfigurationProperties
 public class SmSportApplication {
 
 	public static void main(String[] args) {

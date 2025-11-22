d5cd3afbd6e437af02bba374aa907b22fed1a829
farit <faritteranacosta@gmail.com>
2025-11-21 22:05:50 -0500

    feat: configuracion de jpa

---
+++ src/main/java/com/sm_sport/config/JpaConfig.java
@@ -0,0 +1,9 @@
+package com.sm_sport.config;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
+
+@Configuration
+@EnableJpaAuditing
+public class JpaConfig {
+}

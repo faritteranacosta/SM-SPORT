71b5b9fee0504b1120d10aaa06f68c41c9c74081
farit <faritteranacosta@gmail.com>
2025-11-21 22:06:53 -0500

    fix: Configuracion de openApi

---
+++ src/main/java/com/sm_sport/config/OpenApiConfig.java
@@ -0,0 +1,22 @@
+package com.sm_sport.config;
+
+import io.swagger.v3.oas.models.OpenAPI;
+import io.swagger.v3.oas.models.info.Info;
+import io.swagger.v3.oas.models.info.License;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class OpenApiConfig {
+
+    @Bean
+    public OpenAPI customOpenAPI() {
+        return new OpenAPI()
+                .info(new Info()
+                        .title("API de Mi Proyecto Spring Boot 3.4")
+                        .version("1.0")
+                        .description("Documentación de la API para gestión de recursos")
+                        .termsOfService("https://swagger.io/terms/")
+                        .license(new License().name("Apache 2.0").url("https://springdoc.org")));
+    }
+}
